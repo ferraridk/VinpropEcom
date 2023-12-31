e2e42791bc4dfe479a0c1751051bb774f9721681
ferraridk <henrikferrari@gmail.com>
2023-12-31 01:53:48 +0100

    Identity and security + bug fixes

--- Client/src/app/components/AppPagination.tsx
+++ Client/src/app/components/AppPagination.tsx
@@ -14,8 +14,8 @@ export default function AppPagination({ metaData, onPageChange }: Props) {
         Viser {(currentPage - 1) * pageSize + 1}-
         {currentPage * pageSize > totalCount
           ? totalCount
-          : currentPage * pageSize}
-        af {totalCount} produkter
+          : currentPage * pageSize}{" "}
+        ud af {totalCount} produkter
       </Typography>
       <Pagination
         color="secondary"
